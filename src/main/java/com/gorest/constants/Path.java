d62fc11c613b1413c190ffdd8e0b60b12c229764
preety4697 <147104856+preety4697@users.noreply.github.com>
2024-02-09 18:05:18 +0000

    Week22

---
+++ src/main/java/com/gorest/constants/Path.java
@@ -0,0 +1,9 @@
+package com.gorest.constants;
+
+public class Path {
+    // This path for User API
+    public static final String USERS = "/users";
+
+    // This path for post API
+    public static final String POSTS = "/posts";
+}
---
+++ src/test/java/com/gorest/steps/MyStepdefs.java
@@ -0,0 +1,38 @@
+package com.gorest.steps;
+
+import com.gorest.usersandpostsinfo.PostsSteps;
+import com.gorest.usersandpostsinfo.UsersSteps;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.restassured.response.ValidatableResponse;
+import net.serenitybdd.annotations.Steps;
+
+public class MyStepdefs {
+    static ValidatableResponse response;
+
+    @Steps
+    PostsSteps postsSteps;
+
+    @Steps
+    UsersSteps usersSteps;
+
+    @When("User sends the GET request for getting information")
+    public void userSendsTheGETRequestForGettingInformation(int postId) {
+       postsSteps.getPostByIdAndVerifyTitle(postId);
+    }
+
+    @Then("User must get back with a valid status code {int}")
+    public void userMustGetBackWithAValidStatusCode(int statusCode) {
+        response.statusCode(statusCode);
+    }
+
+    @When("User sends the GET request for getting user information")
+    public void userSendsTheGETRequestForGettingUserInformation(int userId) {
+        usersSteps.getUserInfoByName(userId);
+    }
+
+    @Then("User must be able to get status code {int}")
+    public void userMustBeAbleToGetStatusCode(int statusCode) {
+        response.statusCode(statusCode);
+    }
+}
